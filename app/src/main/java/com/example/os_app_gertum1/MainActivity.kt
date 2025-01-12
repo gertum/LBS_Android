194a2882d0510e2479d516da2c1f617a5c28bb6a
Gerda Tumelytė <gertum1@ktu.lt>
2025-01-12 18:00:38 +0200

    removed unnecessary activities and fragments

--- app/src/main/java/com/example/os_app_gertum1/MainActivity.kt
+++ app/src/main/java/com/example/os_app_gertum1/MainActivity.kt
@@ -2,37 +2,13 @@ package com.example.os_app_gertum1
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.os_app_gertum1.data.database.AppDatabase
-import com.example.os_app_gertum1.data.network.ApiService
-import com.example.os_app_gertum1.data.repository.MeasurementRepository
-import com.example.os_app_gertum1.data.repository.SignalStrengthRepository
 import com.example.os_app_gertum1.service.DataFetchService
 import com.example.os_app_gertum1.ui.SignalMapFragment
-import com.example.os_app_gertum1.ui.map.MapFragment
-import com.example.os_app_gertum1.ui.signalstrengthlist.SignalStrengthListFragment
 import com.example.os_app_gertum1.ui.useridentification.UserIdentificationFragment
 import com.example.os_app_gertum1.ui.usermeasurementlist.UserMeasurementListFragment
 import com.example.os_app_gertum1.utils.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.security.KeyManagementException
-import java.security.NoSuchAlgorithmException
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import java.util.concurrent.TimeUnit
-import javax.net.ssl.SSLContext
-import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
 
